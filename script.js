430a8efd773cf33e26e0a10408fb84cb661b8c60
MarioAntony <mario1995304@gmail.com>
2019-02-06 22:32:08 +0530

    soundCloud

---
+++ script.js
@@ -0,0 +1,223 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+
+});
+
+
+$(document).ready(function() {
+  // KAVAN OX
+  SC.stream('/tracks/298622476', function(sound) {
+    $('#start').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  // IRUTHISUTTU EI SANDAKARA
+
+  SC.stream('/tracks/273618851', function(sound) {
+    $('#start1').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop1').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  // imaikka nodikal neyum nanum anbe
+
+
+  SC.stream('/tracks/568670007', function(sound) {
+    $('#start2').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop2').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  // kadal moonkil thottam
+
+  SC.stream('/tracks/310866390', function(sound) {
+    $('#start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop3').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  // sethupathi konchipesida venam
+
+  SC.stream('/tracks/303954855', function(sound) {
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop4').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // kaththi pakkam vanthu muththam
+
+
+  SC.stream('/tracks/168166232', function(sound) {
+    $('#start5').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop5').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // vikiram vedha tasakku tasakku
+
+
+  SC.stream('/tracks/343795121', function(sound) {
+    $('#start6').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop6').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // thanioruvan kannalaa kannalaa
+
+
+  SC.stream('/tracks/295549427', function(sound) {
+    $('#start7').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop7').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  //vettivel unna pola oruthana
+
+  SC.stream('/tracks/337278082', function(sound) {
+    $('#start8').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop8').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  // purijatha puthir malaikulle
+
+
+  SC.stream('/tracks/337276143', function(sound) {
+    $('#start9').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop9').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // paruththiveeran iyaijioo iyaijoo
+
+
+  SC.stream('/tracks/315708390', function(sound) {
+    $('#start10').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop10').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // thesinkuraja ammadii ammadii
+
+
+
+  SC.stream('/tracks/315401304', function(sound) {
+    $('#start11').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop11').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  // aakko enakena jarum illaije
+  SC.stream('/tracks/191214159', function(sound) {
+      $('#start12').click(function(e) {
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop12').click(function(e) {
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+
+
+    // naanum rowdythn kanaanaa kannee
+
+    SC.stream('/tracks/258015308', function(sound) {
+        $('#start13').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+        $('#stop13').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+      });
+
+
+    // kanaa vaajadi peththaa pulla
+    SC.stream('/tracks/524553387', function(sound) {
+        $('#start14').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+        $('#stop14').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+      });
+
+    // pyar prema kathal  high on love
+
+    SC.stream('/tracks/456435057', function(sound) {
+        $('#start15').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+        $('#stop15').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+      });
+
+});
